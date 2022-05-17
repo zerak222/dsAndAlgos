3234135ba31628cf185d2985b6d0eddba90994a2
Rakesh Nagapuri <Rakesh.Nagapuri@planonsoftware.com>
2022-05-17 23:34:07 +0530

    MaxSumSubArray

--- src/com/arrays/programs/MaxSumSubArray.java
+++ src/com/arrays/programs/MaxSumSubArray.java
@@ -9,15 +9,15 @@ public class MaxSumSubArray {
 
 	public static void main(String[] args) {
 
-		int arr[] = {5,4,-1,7,8};
-		//********************[5,4,-1,7,8]********************
+		int arr[] = {-1,0,-2};
+		//********************[5,4,-1,7,8]********************O/p : 23 ****
 		// 5 -- 5,4 -- 5,4,-1 -- 5,4,-1 -- 5,4,-1,7 -- 5,4,-1,7,8
 		// 4 -- 4,-1  -- 4,-1,7 -- 4,-1,7,8
 		//-1 -- -1,7  -- -1,7,8 
 		// 7 -- 7,8
 		// 8 
 		
-		//********************{-2,1,-3,4,-1,2,1,-5,4}********************
+		//********************{-2,1,-3,4,-1,2,1,-5,4}********************O/p : 6 ****
 		//{-2}  -- {-2,1} -- {-2,1,-3} -- {-2,1,-3,4} -- {-2,1,-3,4,-1} -- {-2,1,-3,4,-1,2} -- {-2,1,-3,4,-1,2,1} -- {-2,1,-3,4,-1,2,1,-5} -- {-2,1,-3,4,-1,2,1,-5,4}
 		//{1}   -- {1,-3} -- {1,-3,4} -- {1,-3,4,-1} -- {1,-3,4,-1,2} -- {1,-3,4,-1,2,1} -- {1,-3,4,-1,2,1,-5} -- {1,-3,4,-1,2,1,-5,4}
 		//{-3}  -- {-3,4} -- {-3,4,-1} -- {-3,4,-1,2} -- {-3,4,-1,2,1} -- {-3,4,-1,2,1,-5} -- {-3,4,-1,2,1,-5,4}
@@ -28,7 +28,14 @@ public class MaxSumSubArray {
 		//{-5}  -- {-5,4}
 		//{4}
 		
+		//*********************{-2,1}********************O/p : 1 *****
+		// {-2} {-2,1}
+		// {1}
 		
+		//*********************{-1,0,-2}********************O/p : 0 *****
+		//{-1} {-1,0} {-1,0,-2}
+		//{0} {0,-2}
+		//{-2}
 		
 		System.out.println("Input : "+Arrays.toString(arr));
 		if(arr==null || arr.length==0) {
@@ -42,17 +49,21 @@ public class MaxSumSubArray {
 	
 	private static void firstMethod(int[] arr) {
 		System.out.println("*******************Simple iteration**********************");
-		int maxSum=arr[0];
+		int finalSum=arr[0];
 		for(int i=0;i<arr.length;i++) {
 			int cumulativeSum=arr[i];
+			int maxSum=arr[i];
 			for(int j=i+1;j<arr.length;j++) {
 				cumulativeSum += arr[j];
 				if(cumulativeSum>maxSum) {
 					maxSum = cumulativeSum;
-				}				
-			}			
+				}
+			}	
+			if(maxSum>finalSum) {
+				finalSum = maxSum;
+			}
 		}
-		System.out.println("Max sum of SubArray : "+maxSum);
+		System.out.println("Max sum of SubArray : "+finalSum);
 	}
 
 }
