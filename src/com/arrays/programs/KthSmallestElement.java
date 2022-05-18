d65ddeca59db58bb0998c39cf53ee98d8600e608
Rakesh Nagapuri <Rakesh.Nagapuri@planonsoftware.com>
2022-05-18 22:31:06 +0530

    KthSmallestElement

--- src/com/arrays/programs/KthSmallestElement.java
+++ src/com/arrays/programs/KthSmallestElement.java
@@ -2,7 +2,7 @@ package com.arrays.programs;
 
 import java.util.Arrays;
 
-public class KthLargestElement {
+public class KthSmallestElement {
 
 	public static void main(String[] args) {
 		int arr[] = {1,2,3,4,5,6};
@@ -13,24 +13,24 @@ public class KthLargestElement {
 			return;
 		}
 		
-		System.out.println("KthLargestElement : "+findKthLargest(arr, k));
+		System.out.println("KthSmallestElement : "+findKthSmallest(arr, k));
 
 	}
 
-	public static int findKthLargest(int[] nums, int k) {
+	public static int findKthSmallest(int[] arr,  int k) {
 		
 		// bubble sorting 
-	    int arrayLength = nums.length;
+	    int arrayLength = arr.length;
 		for(int i=0;i<arrayLength;i++) {
 	    	   for(int j=0;j<arrayLength-i-1;j++) {	    		   
-	    		   if(nums[j]>nums[j+1]) {
-	    			   int temp = nums[j];
-	    			   nums[j]=nums[j+1];
-	    			   nums[j+1]=temp;	
+	    		   if(arr[j]>arr[j+1]) {
+	    			   int temp = arr[j];
+	    			   arr[j]=arr[j+1];
+	    			   arr[j+1]=temp;	
 	    		   }
 	    	   }
 	    	  
 	       }	    
-		return nums[arrayLength-k];
+		return arr[k-1];
     }
 }
