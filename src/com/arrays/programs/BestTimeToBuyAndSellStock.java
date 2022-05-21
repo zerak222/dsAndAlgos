1d4e7c88054de7a7fe9573b15a551cd2cca1c2ef
Rakesh Nagapuri <Rakesh.Nagapuri@planonsoftware.com>
2022-05-21 22:20:08 +0530

    BestTimeToBuyAndSellStock part2

--- src/com/arrays/programs/BestTimeToBuyAndSellStock.java
+++ src/com/arrays/programs/BestTimeToBuyAndSellStock.java
@@ -1,25 +1,21 @@
 package com.arrays.programs;
 
-import java.util.Arrays;
-
 public class BestTimeToBuyAndSellStock {
 
 	public static void main(String[] args) {
 
-		int arr[]= {7,6,4,3,1};
-		
-		
-		System.out.println(Arrays.toString(arr));
+		int arr[]= {7,1,5,3,6,4};
 		
 		int maxProfit = 0;
 		int interimProfit = 0;
-		for(int i=0;i<arr.length;i++) {						
-			for(int j=i+1;j<arr.length;j++) {
-				if(arr[i]<arr[j]) {
-					System.out.println(arr[i]+"-"+arr[j]);
-					interimProfit=arr[j]-arr[i];
-					if(interimProfit>maxProfit) {
-						maxProfit = interimProfit;
+		for(int i=0;i<arr.length;i++) {	
+			if(i+1<arr.length && arr[i]<arr[i+1]) {
+				for(int j=i+1;j<arr.length;j++) {
+					if(arr[i]<arr[j]) {
+						interimProfit=arr[j]-arr[i];
+						if(interimProfit>maxProfit) {
+							maxProfit = interimProfit;
+						}
 					}
 				}
 			}
